c166d3139b703685dd00d49f3d11fd1b27d616f8
Jeff Lewis <jeffylew77@gmail.com>
2018-11-12 15:54:25 -0800

    [Dev] Adding Header + footer

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// Imports: Dependencies
+import React from 'react'
+
+// Component: Header
+const Footer = () => (
+  <div id="header">
+    <img src="/static/ForgetFoamLogo.png"></img>
+  </div>
+)
+
+// Exports
+export default Footer

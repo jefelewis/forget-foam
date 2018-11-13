b66f1e6d29c5c9f93b67529a3612179edceaa8e1
Jeff Lewis <jeffylew77@gmail.com>
2018-11-13 15:46:18 -0800

    [Dev] Adding Ordinance component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react'
 
 // Imports: CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './css/app.css'
 import './css/footer.css'
 import './css/header.css'
 
 // Imports: Components
+
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Ordinance from './components/Ordinance'
 
 // React Application
 const App = () => (
@@ -16,6 +19,7 @@ const App = () => (
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css" />
     <Header />
     {/* <Footer /> */}
+    <Ordinance />
   </div>
 )
 
---
+++ src/components/Ordinance.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-lone-blocks */
+// Imports: Dependencies
+import React, { Component } from 'react'
+
+// Recreational State Data
+const recreationalStates = [
+  'Alaska (AK)',
+  'California (CA)',
+  'Colorado (CO)',
+  'Massachusetts (MA)',
+  'Maine (ME)',
+  'Michigan (MI)',
+  'Nevada (NV)',
+  'Oregon (OR)',
+  'Vermont (VT)',
+  'Washington (WA)',
+]
+
+const medicinalStates = [
+  'Alaska (AK)',
+  'Arkansas (AR)',
+  'Arizona (AZ)',
+  'California (CA)',
+  'Colorado (CO)',
+  'Connecticut (CT)',
+  'Delaware (DE)',
+  'Florida (FL)',
+  'Hawaii (HI)',
+  'Illinois (IL)',
+  'Louisiana (LA)',
+  'Massachusetts (MA)',
+  'Maryland (MD)',
+  'Maine (ME)',
+  'Michigan (MI)',
+  'Minnesota (MN)',
+  'Missouri (MO)',
+  'Montana (MT)',
+  'North Dakota (ND)',
+  'New Hampshire (NH)',
+  'New Jersey (NJ)',
+  'New Mexico (NM)',
+  'Nevada (NV)',
+  'New York (NY)',
+  'Ohio (OH)',
+  'Oklahoma (OK)',
+  'Oregon (OR)',
+  'Pennsylvania (PA)',
+  'Rhode Island (RI)',
+  'Utah (UT)',
+  'Vermont (VT)',
+  'Washington (WA)',
+  'West Virginia (WV)',
+]
+
+// React Component: Ordinance
+class Ordinance extends Component {
+  renderStates (states) {
+    // { states.map(state => <p key={state.id}>{state}</p>) }
+    // { states.map(state => console.log(state))}
+    { return states.map(state => <p key={state}>{state}</p>) }
+  }
+
+  // Render
+  render () {
+    return (
+      <div className="ordinance">
+        <div>
+          { this.renderStates(recreationalStates) }
+        </div>
+      </div>
+    )
+  }
+}
+
+// Exports
+export default Ordinance
